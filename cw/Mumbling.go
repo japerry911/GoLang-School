14f26f413f1ec9d793bfc87aff86a007d807c216
Jack Perry <jack.e.perry@outlook.com>
2022-01-23 16:31:44 -0700

    📦 NEW: new cw problem set up

---
+++ cw/Mumbling.go
@@ -0,0 +1,5 @@
+package main
+
+func Accum(s string) string {
+	return s
+}
--- cw/main.go
+++ cw/main.go
@@ -5,4 +5,7 @@ import "fmt"
 func main() {
 	dnaResult := DNAStrand("ATTGC")
 	fmt.Printf("DNA Passed? %v\n", dnaResult == "TAACG")
+
+	mumblingResult := Accum("abCd")
+	fmt.Printf("Mumbling Passed? %v\n", mumblingResult == "abbCCCdddd")
 }
