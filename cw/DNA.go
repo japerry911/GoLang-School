75dd0fa224db60fb975db4d5775c64e8faf59a50
Jack Perry <jack.e.perry@outlook.com>
2022-01-23 15:06:19 -0700

    👌 IMPROVE: completed cw problem

--- cw/DNA.go
+++ cw/DNA.go
@@ -1,5 +1,32 @@
 package main
 
+import "log"
+
 func DNAStrand(dna string) string {
-	return dna
+	var convertedDnaStrand string
+
+	for _, c := range dna {
+		convertedDnaStrand += convertLetter(c)
+	}
+
+	return convertedDnaStrand
+}
+
+func convertLetter(c rune) string {
+	var convertedLetter string
+
+	switch c {
+	case 'A':
+		convertedLetter = "T"
+	case 'T':
+		convertedLetter = "A"
+	case 'C':
+		convertedLetter = "G"
+	case 'G':
+		convertedLetter = "C"
+	default:
+		log.Fatalf("Invalid DNA character: %v", c)
+	}
+
+	return convertedLetter
 }
